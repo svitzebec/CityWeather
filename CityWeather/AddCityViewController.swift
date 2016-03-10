0cf4452cfe970cc7776e70012ee042c66d4703e8
Svit Zebec <svit.zebec@gmail.com>
2016-03-11 00:01:10 +0100

    Add city screen implementation
    * added AddCityViewController as view controller for the add city screen
    * linked the city list screen with the add city screen

---
+++ CityWeather/AddCityViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddCityViewController.swift
+//  CityWeather
+//
+//  Created by Svit Zebec on 10/03/16.
+//  Copyright © 2016 Svit Zebec. All rights reserved.
+//
+
+import UIKit
+
+class AddCityViewController: UIViewController {
+
+	var cityName: String = ""
+
+	@IBOutlet var cityNameTextField: UITextField!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "doneAddingCity" {
+			cityName = cityNameTextField.text!
+		}
+	}
+
+	override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+		if identifier == "doneAddingCity" {
+			return !cityNameTextField.text!.isEmpty
+		} else {
+			return true
+		}
+	}
+
+	
+}
